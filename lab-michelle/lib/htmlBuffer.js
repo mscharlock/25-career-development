9b0ba11ecaf83c20d2d28ecc61b94dbee8223f5c
mscharlock <michelle.scharlock@gmail.com>
2017-10-19 23:37:27 -0700

    Slightly different idea using fs

--- lab-michelle/lib/htmlBuffer.js
+++ lab-michelle/lib/htmlBuffer.js
@@ -1,7 +1,15 @@
 'use-strict';
 
-module.exports = function HtmlBuffer (buffer) {
-  this.allData = buffer;
-  this.type = buffer.toString('utf-8', 0, buffer.length);
-  //We should think about converting this to some kind of endian here//
-};
+const fs = require('fs');
+let domTree = module.exports = {};
+
+fs.readFile('../assets/minimal.html', (err, data) => {
+  console.log(data);
+});
+
+//
+// module.exports = function HtmlBuffer (buffer) {
+//   this.allData = buffer;
+//   this.type = buffer.toString('utf-8', 0, buffer.length);
+//   //We should think about converting this to some kind of endian here//
+// };
