329137f096a1da5894a8095324c6e63e6c80fbd6
mscharlock <michelle.scharlock@gmail.com>
2017-10-19 23:50:07 -0700

    Weird bug happening in npm?

--- lab-michelle/index.js
+++ lab-michelle/index.js
@@ -17,6 +17,14 @@ fs.readFile('./assets/minimal.html', (err, data) => {
   let stringBuffer = Buffer.from(data);
   console.log(stringBuffer);
   let htmlWords = stringBuffer.toString();
+
+  let splitUpData = htmlWords.split('/n');
+  console.log(splitUpData);
+
+  splitUpData.split('>');
+  console.log(splitUpData);
+
+
   //do we have to split everything up so that the children live in the right spot? Hrmmmm...//
 
   //Things I've tried so far
@@ -29,14 +37,14 @@ fs.readFile('./assets/minimal.html', (err, data) => {
   //maybe we need to make it into a singly linked list to make sure that the kids go in the right spot?? hrmmm
 });
 
-
-let dataFromBuffer = htmlWords;
-//Ok maybe we put all <> things in an arr? A thought
-
-//From the instructions: parse the buffer and use the root <html> element, subsequent HTML tags, and their text content to build a tree data structure
-//hint: you will need to refactor your node to allow a different type of data to be stored in the .value property. The remainder of the node should not change//
-var TreeNode = module.exports = class {
-  constructor(dataFromBuffer) {
-    this.val = dataFromBuffer;
-    this.children = [];
-  }
+//
+// let dataFromBuffer = htmlWords;
+// //Ok maybe we put all <> things in an arr? A thought
+//
+// //From the instructions: parse the buffer and use the root <html> element, subsequent HTML tags, and their text content to build a tree data structure
+// //hint: you will need to refactor your node to allow a different type of data to be stored in the .value property. The remainder of the node should not change//
+// var TreeNode = module.exports = class {
+//   constructor(dataFromBuffer) {
+//     this.val = dataFromBuffer;
+//     this.children = [];
+//   };
--- lab-michelle/lib/htmlBuffer.js
+++ lab-michelle/lib/htmlBuffer.js
@@ -1,15 +1,7 @@
 'use-strict';
 
-const fs = require('fs');
-let domTree = module.exports = {};
-
-fs.readFile('../assets/minimal.html', (err, data) => {
-  console.log(data);
-});
-
-//
-// module.exports = function HtmlBuffer (buffer) {
-//   this.allData = buffer;
-//   this.type = buffer.toString('utf-8', 0, buffer.length);
-//   //We should think about converting this to some kind of endian here//
-// };
+module.exports = function HtmlBuffer (buffer) {
+  this.allData = buffer;
+  this.type = buffer.toString('utf-8', 0, buffer.length);
+  //We should think about converting this to some kind of endian here//
+};
